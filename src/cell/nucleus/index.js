a2a386a59f0c56f727a2040b8f23a92e634532f3
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-28 01:56:12 -0500

    simple scale model

--- src/cell/nucleus/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const nucleus = new THREE.Mesh(
-  new THREE.SphereGeometry(2500, 100, 100),
-  new THREE.MeshBasicMaterial({color: 0x84c2c2, wireframe: true})
-)
-
-export default nucleus
--- src/proteins/cube.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshNormalMaterial()
-)
-
-export default cube 
