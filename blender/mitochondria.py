d67cf3db49766285ca4044dacf1701af6320795c
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-03-10 16:09:21 -0500

    updated cristae blocks script

--- blender/mitochondria.py
+++ blender/mitochondria.py
@@ -2,7 +2,30 @@
 import bpy
 import random
 
+# Length of mitochondrion block
+a = 3
+# Width of mitochondrion block
+b = 1
+# Factor to apply on a to get length of subdived block
+f1 = 0.8
+c = f1 * a
+# Number of cristae rows (#cristae/2)
+n = 20
+# Width of row
+w = c / n
+# Padding in each cristae block
+pf = 0.2
+p = w*pf
+# Width of actual cristae
+wI = w - 4*p
+
 # === Functions ===
+
+# takes two tuples of 3
+def make_box(loc=(0,0,0), scale=(1,1,1)):
+    bpy.ops.mesh.primitive_cube_add(location=loc)
+    bpy.ops.transform.resize(value=scale)
+
 def make_row(n, scale, spacing, origin):
     x = origin["x"]
     y = origin["y"]
@@ -20,8 +43,7 @@ def make_row(n, scale, spacing, origin):
         x += spacing
         
 def make_membranes():
-    bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
-    bpy.ops.transform.resize(value=(3,1,1))
+    make_box((0,0,0), (a,b,1))
     
     # Subsurf modifier
     bpy.ops.object.modifier_add(type="SUBSURF")
@@ -39,7 +61,19 @@ def make_membranes():
     bpy.ops.mesh.separate(type="LOOSE")
 
 # === Main ===
-random.seed(1000825609)        
+random.seed(1000825609)
 make_membranes()
-make_row(20, {"x":0.05}, 0.2, {"x":-2, "y": 0, "z": 0})
+# test f1
+# make_box(scale=(c, 1, 1))
+
+# make_box(loc=(0-c/2,0,0))
+
+start = 0 - c
+for i in range(0, 2*n+1):
+    #x = start + w*i + 4*p*(i+1)
+    #x = start + i*(w+p) + p*2 
+    x = start + w*i
+    make_box(loc=(x,0,0), scale=(wI, 1, 1))
+
+# make_row(20, {"x":0.05}, 0.2, {"x":-2, "y": 0, "z": 0})
  
