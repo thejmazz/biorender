f9909dd31332acf18e93e52e29bd8371de10b71a
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-03-11 01:48:57 -0500

    cristae blocks start off along edge of membrane

--- blender/mitochondria.py
+++ blender/mitochondria.py
@@ -1,6 +1,7 @@
 # === Imports ===
 import bpy, bmesh
 import random
+from operator import itemgetter
 
 # === Functions ===
 
@@ -8,12 +9,12 @@ def numToStr(num):
     if num > 99:
         return str(num)
     elif num > 9:
-        return "0" + str(num)
+        return '0' + str(num)
     else:
-        return "00" + str(num)
+        return '00' + str(num)
 
 def select_some():
-    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.object.mode_set(mode='EDIT')
  
     active_mesh = bpy.context.object.data
     mesh = bmesh.from_edit_mesh(active_mesh)
@@ -22,34 +23,36 @@ def select_some():
     threshold = 0.01
     vertices = []
 
-    for v in mesh.verts:
-        x = v.co.x
-        y = v.co.y
-        z = v.co.z
+    for v in mesh.verts: 
+        v = bpy.context.object.matrix_world * v.co
+
+        x = v.x
+        y = v.y
+        z = v.z
         
         if y >= 0 and z >= center-threshold and z <= center+threshold:
             # v.select = True
             vertices.append({'x': x, 'y':y, 'z':z})
     
-    bpy.ops.object.mode_set(mode="OBJECT")
+    bpy.ops.object.mode_set(mode='OBJECT')
 
     # Trigger viewport update
     #bpy.context.scene.objects.active = bpy.context.scene.objects.active
     
     return vertices
 
-def make_box(loc=(0,0,0), scale=(1,1,1), name=""):
+def make_box(loc=(0,0,0), scale=(1,1,1), name=''):
     bpy.ops.mesh.primitive_cube_add(location=loc)
     bpy.ops.transform.resize(value=scale)
 
-    if name != "":
+    if name != '':
         bpy.context.object.name = name 
 
 
 def make_row(n, scale, spacing, origin):
-    x = origin["x"]
-    y = origin["y"]
-    z = origin["z"]
+    x = origin['x']
+    y = origin['y']
+    z = origin['z']
     
     cristaes = []
     
@@ -57,28 +60,28 @@ def make_row(n, scale, spacing, origin):
         cY = y + (random.random() - 0.5)*3.57
         
         bpy.ops.mesh.primitive_cube_add(radius=1, location=(x,cY,z))
-        bpy.ops.transform.resize(value=(scale["x"], 1, 1))
-        bpy.context.object.name = "Cristae"
+        bpy.ops.transform.resize(value=(scale['x'], 1, 1))
+        bpy.context.object.name = 'Cristae'
         
         x += spacing
         
 def make_membranes(scale, loc=(0,0,0)):
-    make_box(loc, scale, name="Membrane")
+    make_box(loc, scale, name='Membrane')
     
     # Subsurf modifier
-    bpy.ops.object.modifier_add(type="SUBSURF")
-    bpy.context.object.modifiers["Subsurf"].levels = 4
-    bpy.context.object.modifiers["Subsurf"].render_levels = 4
-    bpy.ops.object.modifier_apply(apply_as="DATA", modifier="Subsurf")
+    bpy.ops.object.modifier_add(type='SUBSURF')
+    bpy.context.object.modifiers['Subsurf'].levels = 4
+    bpy.context.object.modifiers['Subsurf'].render_levels = 4
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Subsurf')
     
     # Solidify
-    bpy.ops.object.modifier_add(type="SOLIDIFY")
-    bpy.context.object.modifiers["Solidify"].offset = 0
-    bpy.context.object.modifiers["Solidify"].thickness = 0.04
-    bpy.ops.object.modifier_apply(apply_as="DATA", modifier="Solidify")
+    bpy.ops.object.modifier_add(type='SOLIDIFY')
+    bpy.context.object.modifiers['Solidify'].offset = 0
+    bpy.context.object.modifiers['Solidify'].thickness = 0.04
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Solidify')
     
     # Seperate Inner and Outer membrane
-    bpy.ops.mesh.separate(type="LOOSE")
+    bpy.ops.mesh.separate(type='LOOSE')
 
 def make_mitochondria(loc=(0,0,0), length=3, width=1, num_rows=30, padding_factor=0.2):
     mito_length = 0.8*length
@@ -88,16 +91,23 @@ def make_mitochondria(loc=(0,0,0), length=3, width=1, num_rows=30, padding_facto
     make_membranes(scale=(length, width, 1))
     
     bpy.context.object.select = False
-    bpy.data.objects["Membrane"].select = True
-    bpy.data.objects["Membrane.001"].select = False
-    vertices = select_some()
-    print(vertices)
+    bpy.data.objects['Membrane'].select = True
+    bpy.data.objects['Membrane.001'].select = False
+    
+    vertices = sorted(select_some(), key=itemgetter('x')) 
 
     start = 0 - mito_length
-    for i in range(0, num_rows+1): 
+    for i in range(0, num_rows+1):
         x = start + 2*row_width*i
-        y = 3*(random.random() - 0.5) 
-        make_box(loc=(x, y, 0), scale=(cristae_width, 1, 1), name="Cristae." + numToStr(i))
+        y = -2
+        
+        for v in vertices: 
+            if v['x'] >= x:
+                y = v['y'] + width
+                break
+
+        # y = 3*(random.random() - 0.5) 
+        make_box(loc=(x, y, 0), scale=(cristae_width, 1, 1), name='Cristae.' + numToStr(i))
         bpy.context.object.select = False
 
 # === Main ===
