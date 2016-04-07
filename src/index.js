0c56eddb441ba9199ac0eabf7c76136457e1f916
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-04-07 13:57:55 -0400

    cristae with rim as another obj, and etc/curved as polygroups

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ for (let obj3DKey of Object.keys(sceneGraph)) {
 // ===========================================================================
 
 const OBJLoader = new THREE.OBJLoader()
-console.log(OBJLoader)
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
 
@@ -44,32 +43,6 @@ ground.position.set(0,-2,0)
 ground.rotation.x = Math.PI/2
 scene.add(ground)
 
-OBJLoader.load('/models/disk.obj', (object) => {
-  console.log(object)
-  object.position.set(0,0,5)
-
-  let curved, etc
-
-  object.children.forEach( (child) => {
-    // console.log(child)
-    child.material.side = THREE.DoubleSide
-
-    if (child.name === 'Curved') {
-      child.material = new THREE.MeshLambertMaterial({color: 0xe42908, side: THREE.DoubleSide})
-      curved = child
-    } else if (child.name === 'ETC') {
-      child.material = new THREE.MeshLambertMaterial({color: 0x2141b5})
-      etc = child
-    }
-
-    // if (child.name !== 'Cube')
-    //   scene.add(child)
-  })
-
-  // scene.add(curved)
-  // scene.add(etc)
-})
-
 OBJLoader.load('/models/cristae.obj', (object) => {
   let curved = []
   let rim = []
@@ -78,14 +51,17 @@ OBJLoader.load('/models/cristae.obj', (object) => {
   console.log(object)
 
   object.children.forEach( (child) => {
-    if (child.name === 'Curved') {
+    child.name = child.name.split('_')[0]
+
+    console.log(child.name)
+    if (child.name === 'Cristae.Curved') {
       child.material = new THREE.MeshLambertMaterial({color: 0xe42908, side: THREE.DoubleSide})
       curved.push(child)
-    } else if (child.name === 'ETC') {
-      child.material = new THREE.MeshLambertMaterial({color: 0x2141b5})
+    } else if (child.name === 'Cristae.ETC') {
+      child.material = new THREE.MeshLambertMaterial({color: 0x2141b5, side: THREE.DoubleSide})
       etc.push(child)
-    } else if (child.name === 'Rim') {
-      child.material = new THREE.MeshLambertMaterial({color: 0x6d12e0})
+    } else if (child.name === 'Cristae.Rim') {
+      child.material = new THREE.MeshLambertMaterial({color: 0x6d12e0, side: THREE.DoubleSide})
       rim.push(child)
     }
   })
